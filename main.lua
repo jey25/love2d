13d32a5edc907abc9294bc1b2a1816f9f9c0ef9a
jey25 <jjangeoyoung@gmail.com>
2022-12-13 01:07:17 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,11 @@
+message = 5
+chicken = 10
+output = chicken * 3
+
+message = message - 1
+
+
 function love.draw()
     love.graphics.setFont(love.graphics.newFont(50))
-    love.graphics.print("Hello")
+    love.graphics.print(message)
 end
