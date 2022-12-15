9b251e08797e49d8170f4fd2786f61f49e16da1c
jey25 <jjangeoyoung@gmail.com>
2022-12-16 01:38:31 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -4,10 +4,10 @@ condition = -25
 
 if condition > 0 then
     message = 1
-end
-
-if condition <= 0 then
+elseif condition < -100 then
     message = -1
+else
+    message = "no condition met"
 end
 
 
