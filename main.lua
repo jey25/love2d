d89545895d00da2dd3f3c9be022838a48166bf6d
jey25 <jjangeoyoung@gmail.com>
2022-12-16 01:39:33 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -6,6 +6,9 @@ if condition > 0 then
     message = 1
 elseif condition < -100 then
     message = -1
+
+elseif condition == -25 then
+    message = "right"
 else
     message = "no condition met"
 end
