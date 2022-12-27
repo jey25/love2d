6a5a2115b7ed8e98d80007a140070135f29d008f
jey25 <jjangeoyoung@gmail.com>
2022-12-28 00:08:19 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,12 @@
 message = 0
 
+function increaseMessage(...)
+    message = message + 5
+    -- body
+end
+
+increaseMessage()
+
 function love.draw()
     love.graphics.setFont(love.graphics.newFont(50))
     love.graphics.print(message)
