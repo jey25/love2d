8e13d41039b2aaad7001fb32066776bd2f568206
jey25 <jjangeoyoung@gmail.com>
2022-12-27 23:38:37 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
 message = 0
 test = 0
+pickle = 0
 
 while message < 100 do
     message = message + 1
@@ -7,7 +8,11 @@ while message < 100 do
 
 end
 
+for i = 1, 3, 1 do
+    pickle = pickle + i
+end
+
 function love.draw()
     love.graphics.setFont(love.graphics.newFont(50))
-    love.graphics.print(test)
+    love.graphics.print(pickle)
 end
