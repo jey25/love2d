6c6ae9f85bcfce9c7866f84be0996baa84c996b8
jey25 <jjangeoyoung@gmail.com>
2023-01-20 00:19:55 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,22 @@
 message = 0
 
+testScore = {}
+
+testScore[1] = 95
+
+message = testScore[1]
+
+function jaykim()
+    if size == 3 then
+        return true
+    else
+        return false
+    end
+
+
+end
 
 function love.draw()
-    love.graphics.setFont(love.graphics.newFont(50))
+    love.graphics.setFont(love.graphics.newFont(150))
     love.graphics.print(message)
 end
