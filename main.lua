ef1f55ddf178e987121ddc2f6dd60d1c817b9035
jey25 <jjangeoyoung@gmail.com>
2023-05-09 23:48:49 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,12 +1,16 @@
 
 function love.load()
-    number = 0
+    target = {}
+    target.x = 300
+    target.y = 300
+    target.radius = 50
 end
 
 function love.update( dt )
-    number = number + 1
+ 
 end
 
 function love.draw(  )
-    love.graphics.print(number)
+    love.graphics.setColor(1, 0, 0)
+    love.graphics.circle("fill", target.x, target.y, target.radius)
 end
